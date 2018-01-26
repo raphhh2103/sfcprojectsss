786118bdc77d6988c65c8dd4cc2de28980490724
raphaelk <raphaelk@live.be>
2018-01-26 14:25:02 +0100

    test

--- src/Controller/SkillsController.php
+++ src/Controller/SkillsController.php
@@ -14,6 +14,6 @@ class SkillsController extends Controller
     public function index()
     {
         // replace this line with your own code!
-        return $this->render('@Maker/demoPage.html.twig', [ 'path' => str_replace($this->getParameter('kernel.project_dir').'/', '', __FILE__) ]);
+        return $this->render('@Maker/ demoPage.html.twig', [ 'path' => str_replace($this->getParameter('kernel.project_dir').'/', '', __FILE__) ]);
     }
 }
